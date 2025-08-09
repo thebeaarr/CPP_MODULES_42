6f8af6cfe6c5bac3623819a81d32236904522a05
feeedback <m0h4m3dl4khd4r@gmail.com>
2025-08-09 03:32:03 +0100

    ownedbyhunter

--- cpp-01/ex04/main.cpp
+++ cpp-01/ex04/main.cpp
@@ -2,32 +2,40 @@
 #include <iostream>
 #include <string>
 
-std::string line_c(std::string line, char **argv) {
-    std::string uline;
-    char c;
-    std::string s1(argv[1]);
-    std::string s2(argv[2]);
-    while (c != '\0') {
-        int size = line.find(s1);
-        
-    }
-    return uline;
-}
 int main(int argc, char **argv) {
-    (void)argc;
-    std::ifstream infile(argv[3]);
-    std::string cfile;
-    std::string line;
-    while (std::getline(infile, line)) {
-        std::string update_line;
-        update_line = line_c(line, argv);
-        cfile += update_line;
+
+    if(argc == 4)
+    {
+        std::ifstream infile(argv[3]);
+        std::string s1(argv[1]);
+        std::string s2(argv[2]);
+        std::string cfile;
+        std::string line;
+        while (std::getline(infile, line))
+        {
+            std::string::size_type pos = 0;
+            std::string::size_type found;
+            std::string update_line; // replacing means that we remove s1 and replace by s2
+            while(true)
+            {
+                found = line.find(s1 , pos);
+                if (found == std::string::npos)
+                {
+                    update_line += line.substr(pos);
+                    break;
+                }
+                update_line += line.substr(0 ,found - pos);
+                update_line += s2;
+                pos = found + s1.size();
+            }
+            cfile += update_line + '\n';
+        }
+        infile.close();
+        std::ofstream outfile(argv[3], std::ios::trunc);
+        outfile << cfile;
+        outfile.close();
+        std::cout << cfile;
     }
-    infile.close();
-
-    std::ofstream outfile(argv[3], std::ios::trunc);
-    outfile << cfile;
-    outfile.close();
-
-    std::cout << cfile;
+    else
+        std::cerr << "error.3 params make sure next time." << std::endl;
 }
