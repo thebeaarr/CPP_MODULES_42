393261a881b8223a9d70af1d419fa3d2cda21a5c
Mohamed Lakhdar <mlakhdar@c4r5p3.1337.ma>
2025-08-11 20:14:53 +0100

    mohjamed

--- cpp-02/ex01/Fixed.cpp
+++ cpp-02/ex01/Fixed.cpp
@@ -6,4 +6,36 @@ Fixed::Fixed()
     std::cout << "Default constructor called" << std::endl ;
 }
 
-Fixed::
+Fixed::Fixed(const Fixed &f) {
+    std::cout << "Copy constructor called" << std::endl;
+    *this = f;
+}
+
+Fixed &Fixed::operator=(const Fixed &f) {
+    std::cout << "Copy assignment operator called" << std::endl;
+    this->_value = f._value;
+    return *this;
+}
+
+Fixed::~Fixed() { std::cout << "Destructor called" << std::endl; }
+
+float Fixed::toFloat() const { return (float)_value / (1 << _fraction); }
+
+int Fixed::toInt() const { return _value; }
+
+float &Fixed::operator<<(const Fixed &f) {
+    // return the floiting point of the value.
+    return (float)this->_value;
+}
+
+Fixed::Fixed(int const &i) {}
+
+Fixed::Fixed(float const &f) {
+    std::cout << "Float constructor called" << std::endl;
+    // use fixed point. to convert this.
+}
+
+Fixed::Fixed(int const &i) {
+    std::cout << "Int constructor called" << std::endl;
+    // use fixed point. to convert this.
+}
--- cpp-02/ex01/Fixed.hpp
+++ cpp-02/ex01/Fixed.hpp
@@ -7,12 +7,13 @@ class Fixed
         const static int _fraction;
     public:
         Fixed();
+        Fixed(int const &i);
+        Fixed(float const &f);
         Fixed(const Fixed &f);
         Fixed &operator=(const Fixed &f);
         ~Fixed();
 
-
-        Fixed &operator<<(const Fixed &f);
+        float &operator<<(const Fixed &f);
         float toFloat( void ) const;
         int toInt( void ) const;
 };
