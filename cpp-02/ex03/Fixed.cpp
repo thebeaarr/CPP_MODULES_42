0c4a0eef58af2160604c9435c01a66a0899abab2
Mohamed Lakhdar <mlakhdar@c4r8p3.1337.ma>
2025-08-23 16:23:55 +0100

    problem ex03

--- cpp-02/ex03/Fixed.cpp
+++ cpp-02/ex03/Fixed.cpp
@@ -3,49 +3,49 @@
 
 Fixed::Fixed() : _value(0)
 {
-  std::cout << "Default constructor called" << std::endl;
+  // std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed &copyc): _value(copyc._value)
 {
-  std::cout << "Copy constructor called" << std::endl;
+  // std::cout << "Copy constructor called" << std::endl;
 }
 
 int Fixed::getRawBits() const
 {
-  std::cout << "getRawBits member function called" << std::endl;
+  // std::cout << "getRawBits member function called" << std::endl;
   return _value;
 }
 
 Fixed &Fixed::operator=(const Fixed &op)
 {
   this->_value = op._value;
-  std::cout << "Copy assignment operator called" << std::endl;
+  // std::cout << "Copy assignment operator called" << std::endl;
   return *this;
 }
 
 void Fixed::setRawBits(int const raw)
 {
-  std::cout << "setRawBits member function called" << std::endl;
+  // std::cout << "setRawBits member function called" << std::endl;
   this->_value = raw;
 }
 
 
 Fixed::~Fixed()
 {
-  std::cout << "Destructor called" << std::endl;
+  // std::cout << "Destructor called" << std::endl;
 }
 
 Fixed::Fixed(const int &value)
 {
   this->_value = value <<  _fracbits;
-  std::cout << "Int constructor called" << std::endl;
+  // std::cout << "Int constructor called" << std::endl;
 }
 
 Fixed::Fixed(const float &value)
 {
   this->_value = (int)roundf(value * (1 << _fracbits));
-  std::cout << "Float constructor called" << std::endl;
+  // std::cout << "Float constructor called" << std::endl;
 }
 
 float Fixed::toFloat() const
@@ -66,28 +66,28 @@ std::ostream &operator<<(std::ostream &out , const Fixed &op)
 }
 
 // arithmetic operators
-Fixed Fixed::operator*(const Fixed &op)
+Fixed Fixed::operator*(const Fixed &op) const
 {
   Fixed tmp ;
   tmp._value = (this->_value * op._value) / ( 1 << this->_fracbits);
   return tmp;
 }
 
-Fixed Fixed::operator/(const Fixed &op)
+Fixed Fixed::operator/(const Fixed &op) const
 {
   Fixed tmp ;
   tmp._value = (this->_value << this->_fracbits) / op._value;
   return tmp;
 }
 
-Fixed Fixed::operator+(const Fixed &op)
+Fixed Fixed::operator+(const Fixed &op) const
 {
   Fixed tmp ;
   tmp._value = this->_value + op._value;
   return tmp;
 }
 
-Fixed Fixed::operator-(const Fixed &op)
+Fixed Fixed::operator-(const Fixed &op) const
 {
   Fixed tmp ;
   tmp._value = this->_value - op._value;
