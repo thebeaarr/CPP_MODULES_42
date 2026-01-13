c069fe59dbf9f937f75f4d61e0b9110dab73e5a6
Mohamed Lakhdar <m0h4m3dl4khd4r@gmail.com>
2026-01-13 02:36:43 +0100

    ghir knkhrbq db

--- cpp_module07/ex02/Array.hpp
+++ cpp_module07/ex02/Array.hpp
@@ -1,1 +1,31 @@
-// file 
+#pragma  once 
+
+
+#include <string>
+#include <iostream>
+
+template <typename T>
+class Array
+{
+	private :
+		T *a ;
+	public :
+		Array();
+		Array(unsigned int n) 
+		{
+			a = new T[n];
+		}
+		
+		Array &operator=(const Array &obj)
+		{
+			if(this != &obj)
+			{
+				this->a = new T[](obj.a);
+			}
+			return *this ;
+		}
+		Array(const Array &obj)
+		{
+			
+		}
+};
